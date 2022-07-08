3857ccf0070698eb5afca4193a92215f27267918
vcsil <vcsilva@poli.ufrj.br>
2022-07-08 00:02:02 -0300

    Adiciona rotas relacionada a autentificação do usuário
    Cria rotas, middlewares, schemas e os controlles

---
+++ src/routes/authRouter.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+import { signIn, signUp } from "../controllers/authController.js";
+import userSignInSchemaValidationMiddleware from "../middlewares/userSignInSchemaValidationMiddleware.js";
+import userSignUpSchemaValidationMiddleware from "../middlewares/userSignUpSchemaValidationMiddleware.js";
+
+const authRouter = Router();
+
+authRouter.post("/sign-up", userSignUpSchemaValidationMiddleware, signUp);
+authRouter.get("/sign-up", (req, res) => {
+    res.send("Vai vendo - sign-up");
+});
+authRouter.post("/sign-in", userSignInSchemaValidationMiddleware, signIn);
+authRouter.get("/sign-in", (req, res) => {
+    res.send("Vai vendo - sign-in");
+});
+
+export default authRouter;
