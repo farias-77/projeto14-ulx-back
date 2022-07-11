476b7afee2cc4a19eeade0a9b9cb6ae04d651dc2
daniel <daniel.bnh13@gmail.com>
2022-07-11 12:20:03 -0300

    criacao do historico

--- src/routes/cartproductRouter.js
+++ src/routes/cartproductRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { creation,cart,cartdelete,historic,YourProduct, yourdelete} from "../controllers/cartproductController.js";
+import { creation,cart,cartdelete,historic,YourProduct, yourdelete,creationHistoric} from "../controllers/cartproductController.js";
 import creationValidation  from "../middlewares/creationValidation.js";
 import cartValidation  from "../middlewares/cartValidation.js";
 
@@ -10,6 +10,8 @@ cartproductRouter.post("/creation", creationValidation ,creation);
 cartproductRouter.post("/cart",cart);
 cartproductRouter.post("/YourProduct",YourProduct);
 cartproductRouter.post("/historic",historic);
+cartproductRouter.post("/creationHistoric",creationHistoric);
 cartproductRouter.post("/cartdelete",cartdelete);
 cartproductRouter.post("/yourdelete",yourdelete);
+
 export default cartproductRouter;
